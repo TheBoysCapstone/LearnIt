6e109cec2b9c5de29e8088baca898608107292ec
berezovs <arthas2103@LAPTOP-P78N46FO.localdomain>
2022-02-10 12:54:40 -0600

    created a react app and started working on the login page

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from "./Components/Login.js"
+
+
+function App() {
+  return (
+    <div className="container">
+      <Login />
+    </div>
+      
+    
+  );
+}
+
+export default App;

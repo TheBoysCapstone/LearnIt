e2397249257d071bd0a2e5b97b402782601d27fe
berezovs <arthas2103@LAPTOP-P78N46FO.localdomain>
2022-02-13 02:38:19 -0600

    finished Login and Signup Screens. TODO: start working on the serverside code

--- frontend/react-app/src/Components/Login.js
+++ frontend/react-app/src/Components/Login.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () =>{
+    const[loginInfo, setLoginInfo] = useState({username:'', password: ''})
+    const[warning, setWarning] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setWarning('');
+
+        if(loginInfo.username && loginInfo.password){
+            //code that sends loginInfo to the server goes here
+
+            setLoginInfo({username:'', password: ''})
+        }else{
+            setWarning("All fields need to be completed")
+        }
+    }
+
+    const handleChange = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        setLoginInfo({...loginInfo, [field]: value})
+    }
     return(
         <>
             <h4>Login to continue</h4>
             <form className="form">
-                
+                <div>
+                    <h4 className="warning">{warning}</h4>
+                </div>
                 <div>
                     <label htmlFor="username">Username</label>
-                    <input type='text' id='username' name='username' size="30"/>
+                    <input type='text' id='username' name='username' size="30" value={loginInfo.username} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type='text' id='password' name='password' size="30"/>
+                    <input type='password' id='password' name='password' size="30" value={loginInfo.password} onChange={handleChange}/>
                 </div>
-                <button type="submit" className="btn btn-login">Login</button>
+                <button type="submit" className="btn btn-login" onClick={handleSubmit}>Login</button>
             </form>
             
             
--- frontend/react-app/src/Components/Signup.js
+++ frontend/react-app/src/Components/Signup.js
@@ -3,27 +3,48 @@ import React, {useState} from 'react'
 
 const Signup = () =>{
     const[signupInfo, setSignupInfo]=useState({username:'', email:'', password:''})
+    const[warning, setWarning] = useState("");
 
-    const handleSignup = (e) => {
+    const handleChange = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        setSignupInfo({...signupInfo, [field]: value});
+        console.log(signupInfo);
+    }
 
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setWarning("")
+        if(signupInfo.username && signupInfo.email && signupInfo.password){
+
+            //code that will send signUpInfo object to the server goes here
+
+            setSignupInfo({username: '', email:'', password: ''})
+        }
+        else{
+            setWarning("All fields need to be filled out")
+        }
     }
     return(
         <>
             <h4>Create an account</h4>
             <form className="form">
                 <div>
+                    <h4 className="warning">{warning}</h4>
+                </div>
+                <div>
                     <label htmlFor="username">Username</label>
-                    <input type='text' id='username' name='username' size="30"/>
+                    <input type='text' id='username' name='username' size="30"  value={signupInfo.username} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor='email' >Email</label>
-                    <input type='email' id='email' name='email' size="30"/>
+                    <input type='email' id='email' name='email' size="30" value={signupInfo.email} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type='text' id='password' name='password' size="30"/>
+                    <input type='password' id='password' name='password' size="30" value={signupInfo.password} onChange={handleChange}/>
                 </div>
-                <button type="submit" className="btn btn-signup">Register</button>
+                <button type="submit" className="btn btn-signup"  onClick={handleSubmit}>Register</button>
             </form>
             
             

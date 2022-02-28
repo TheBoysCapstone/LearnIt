b7fff68ff586c68a688d30851009151e1ecf04c1
Inviber <bkelley4600@gmail.com>
2022-02-27 20:55:24 -0600

    It inserts a user into the db, but not sure why it keeps timing out for the tests

--- tests/mongodb-users.test.js
+++ tests/mongodb-users.test.js
@@ -1,6 +1,7 @@
 // Test if the database creates/stores certain users
 
 const {MongoClient} = require('mongodb');
+const jestConfig = require('./jest-config');
 const url = "mongodb+srv://theboys:learnit_password@learnit.9tsp5.mongodb.net/test?retryWrites=true&w=majority";
 
 describe('insert', () => {
@@ -16,22 +17,21 @@ describe('insert', () => {
           db = await connection.db();
     });
 
+    // Cleans the test database before the test runs and disconnects from db
     afterAll(async () => {
+        await db.collection('users').deleteMany({});
         await connection.close();
     });
-
-    // Cleans the test database after the test runs
-    // afterEach(async () => {
-    //     await db.collection('users').deleteMany({});
-    // });
 
     it('Should insert a user into database', async done => {
         const users = db.collection('users');
 
-        const mockUser = {username: 'mock', password: 'mockPassword', email: 'mock@gmail.com'};
+        const mockUser = {_id: 'some-user-id', username: 'mock', password: 'mockPassword', email: 'mock@gmail.com'};
         await users.insertOne(mockUser);
 
-        const insertedUser = await testUsers.findOne({username: 'mock'});
-        expect(insertedUser).toEqual(mockUser);
+        const insertedUser = await users.findOne({_id: 'some-user-id'});
+        console.log(insertedUser);
+        console.log(mockUser);
+        expect(mockUser).toEqual(insertedUser);
     });
 });

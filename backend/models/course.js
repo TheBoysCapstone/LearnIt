289c0d149a3f6b381c45f9144311a0d2580e9961
berezovs <sb206@students.uwf.edu>
2022-03-22 15:33:45 -0500

    Implemented the create-course route and made sure it saves course and its questions in the respective collections in the database

---
+++ backend/models/course.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const course = new mongoose.Schema({
+    title: String,
+    body: String,
+    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+},
+{collection: 'courses'})
+
+module.exports = mongoose.model("Courses", course)
---
+++ backend/models/question.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const question = new mongoose.Schema({
+    question: String,
+    isCorrect: Boolean,
+    courseID: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+},
+{collection: 'questions'})
+
+module.exports = mongoose.model("Questions", question)

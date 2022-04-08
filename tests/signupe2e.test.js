e3929f234cf60feb5d3331d02e2d9eb7acf6faf3
Inviber <bkelley4600@gmail.com>
2022-04-07 20:24:49 -0500

    Test for checking if the right header text is on the Signup page

--- tests/signupe2e.test.js
+++ tests/signupe2e.test.js
@@ -1,16 +1,18 @@
 const puppeteer = require('puppeteer');
 
 describe('signup page', () => {
-    
+    let browser;
+    let page;
+
     beforeAll( async () => {
-        const browser = await puppeteer.launch({ headless: false, ignoreDefaultArgs: ['--disable-extensions'] });
-        const page = await browser.newPage();
+        browser = await puppeteer.launch({ headless: false, ignoreDefaultArgs: ['--disable-extensions'] });
+        page = await browser.newPage();
         await page.goto('http://localhost:3000', {waitUntil: 'domcontentloaded'});
     });
 
     it('should show the signup text', async () => {
-        const headerHandle = await page.$('.login');
-        const html = await page.evaluate(headerHandle => headerHandler.innerHTML, headerHandle);
-        expect(html).toBe('Login to continue'); 
+        const headerHandle = await page.$('.signup-header');
+        const html = await page.evaluate(headerHandle => headerHandle.innerHTML, headerHandle);
+        expect(html).toBe('Create an account'); 
     });
 });
